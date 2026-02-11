eff4eacb2b929c8018c52aaba086e44aca1ed741
AnMaLeNo <antoineblr16@gmail.com>
2026-02-11 18:48:49 +0100

    feat: Implement connection retry for Postgres client

--- auth-api/src/database/db.js
+++ auth-api/src/database/db.js
@@ -2,35 +2,41 @@ const postgres = require("postgres");
 
 const sql = postgres({
   host                 : 'databse', // hard coding un peut
-  port                 : 5432, // ?
+  port                 : 5432,
   database             : process.env.AUTH_DB,
   username             : process.env.AUTH_DB_USER,
   password             : process.env.AUTH_DB_PASSWORD,
 });
 
+const sleep = (ms) => { return new Promise((resolve) => {setTimeout(resolve, ms)}) };
+
 async function initDb() {
-    try {
-        await sql.begin(async (sql) => { 
-            
-            await sql`
-            CREATE TABLE IF NOT EXISTS users (
-                id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-                email TEXT NOT NULL UNIQUE,
-                password_hash TEXT NOT NULL
-            );
-            `;
+    for (let t = 1; t < 5; t++) {
+        try {
+            await sql.begin(async (sql) => { 
+                
+                await sql`
+                CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                    email TEXT NOT NULL UNIQUE,
+                    password_hash TEXT NOT NULL
+                );
+                `;
 
-            await sql`
-                CREATE INDEX IF NOT EXISTS idx_users_email ON users(email);
-            `;
-        });
+                await sql`
+                    CREATE INDEX IF NOT EXISTS idx_users_email ON users(email);
+                `;
+            });
 
-        console.log('Schéma de base de données vérifié et initialisé.');
+            console.log('Schéma de base de données vérifié et initialisé.');
+            return ;
 
-    } catch (error) {
-        console.error('Erreur critique DDL (Data Definition Language) :', error);
-        process.exit(1);
+        } catch (error) {
+            console.error('Erreur critique DDL (Data Definition Language) :', error);
+        }
+        await sleep(1000 * t);
     }
+    process.exit(1);
 }
 
 module.exports = {sql, initDb};
