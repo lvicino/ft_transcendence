f22aeb053ae720e94c40113555ebd7e93e0117b7
Antoine Monot <amonot@f6r6s11.clusters.42paris.fr>
2026-03-22 20:04:25 +0100

    feat: add input for id and password

--- frontend/src/pages/Lobby.tsx
+++ frontend/src/pages/Lobby.tsx
@@ -5,28 +5,45 @@ import { useTranslation } from 'react-i18next';
 import { Button } from '../components/ui/Button';
 import { useGameFlowStore } from '../store/gameStore';
 
+import { Input } from "@/components/ui/Input";
+
 export default function Lobby() {
   const navigate = useNavigate();
   const { t } = useTranslation();
 
   const matchId = useGameFlowStore((s) => s.matchId);
+  const password = useGameFlowStore((s) => s.password);
   const startMatch = useGameFlowStore((s) => s.startMatch);
   const leaveLobby = useGameFlowStore((s) => s.leaveLobby);
+  const setpassword = useGameFlowStore((s) => s.setpassword);
+  const setmatchId = useGameFlowStore((s) => s.setmatchId);
 
   return (
     <div className="mx-auto w-full max-w-md space-y-6 py-10">
 
       <h1 className="text-3xl font-bold text-white">{t("lobbyTitle")}</h1>
 
-      <div className="text-sm text-white/60">
-        Match ID: <span className="text-white">{matchId}</span>
-      </div>
+			<Input
+				placeholder={"Match ID"}
+				name="matchID"
+				value={matchId ?? ""}
+				onChange={(e) => !Number.isNaN(Number(e.target.value)) ? setmatchId(Number(e.target.value)) : null}
+				className="font-mono text-center bg-black/50 border-white/10 focus:border-white/30"
+			/>
+
+			<Input
+				placeholder={"Match password"}
+				name="password"
+				value={password ?? ""}
+				onChange={(e) => setpassword(e.target.value)}
+				className="font-mono text-center bg-black/50 border-white/10 focus:border-white/30"
+			/>
 
       <Button
         className="w-full"
         onClick={() => {
           startMatch();
-          navigate(`/game/${matchId}`);
+          navigate(`/game`);
         }}
       >
         {t("startMatch")}
